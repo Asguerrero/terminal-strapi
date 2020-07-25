b027d525c157fb329b82662afbd6850dcc593665
Asguerrero <valentinaguerrero.as@hotmail.com>
2020-07-25 12:28:43 -0500

    Update database config

--- config/database.js
+++ config/database.js
@@ -1,18 +1,22 @@
-module.exports = ({ env }) => ({
+module.exports = ({ env }) => (
+  {
   "defaultConnection": "default",
   "connections": {
     "default": {
       "connector": "bookshelf",
       "settings": {
         "client": "postgres",
-        "host": "0.0.0.0",
-        "port": "${process.env.DATABASE_PORT}",
-        "database": "${process.env.DATABASE_NAME}",
-        "username": "${process.env.DATABASE_USERNAME}",
-        "password": "${process.env.DATABASE_PASSWORD}",
-        "ssl": { "rejectUnauthorized": false }
+        "host": "ec2-52-22-216-69.compute-1.amazonaws.com",
+        "port": 5432,
+        "username": "axlyjrhzmyrllt",
+        "password": "307f720c4e73b826294a675dce6f93e14aa2a2157e66c03e8389a3efc5ccb939",
+        "database": "d4h8lctmnlikt4",
+        "schema": "public"
       },
-      "options": {}
+      "options": {
+        "debug": true
+      }
     }
   }
-});
+}
+  );
---
+++ config/plugins.js
@@ -0,0 +1,13 @@
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    provider: 'cloudinary',
+    providerOptions: {
+      cloud_name: 'dvux1btg4',
+      api_key: '532929477424613',
+      api_secret: 'IHkT3rPuceXnqHY5VibBcw6Z8xQ',
+    },
+  },
+  // ...
+});
+
