348921f4c2701f136440397457f27009936df070
Asguerrero <valentinaguerrero.as@hotmail.com>
2020-07-31 14:20:46 -0500

    Cross my fingers

--- config/database.js
+++ config/database.js
@@ -1,24 +1,29 @@
-module.exports = ({ env }) => (
-  {
-  "defaultConnection": "default",
-  "connections": {
-    "default": {
-      "connector": "bookshelf",
-      "settings": {
-        "client": "postgres",
-        "host": "ec2-107-20-104-234.compute-1.amazonaws.com",
-        "port": 5432,
-        "username": "xhsfggbmzoguml",
-        "password": "f621529f161cf1f77539a0fc635dfdbac5fcb74dda8534e368b896fad23db179",
-        "database": "d9fcqfoqphps8k",
-        "schema": "public"
-      },
-      "options": {
-        "debug": true
-      }
-    }
-  }
-}
-  );
+let newregex = /[:@/]/g;
+let regex = new RegExp(newregex);
+let url = env('DATABASE_URL');
+let text = url.split(regex)
 
+
+
+module.exports = ({ env }) => (
+
+
+{
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: String(text[5]),
+        port: String(text[6]),
+        database: String(text[7]),
+        username: String(text[3]),
+        password: String(text[4]),
+        schema: 'public',
+      },
+      options: {},
+    },
+  },
+});
 
